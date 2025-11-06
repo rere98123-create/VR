ff89a9d446765ee0c036464c8ca237d72453d51a
rere98123-create <rere98123@gmail.com>
2025-11-06 11:42:11 +0800

    123

---
+++ 池袋/data.js
@@ -0,0 +1,270 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-35",
+      "name": "池袋駅35番出口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -0.7141602347446181,
+        "pitch": -0.012719103519504671,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3501483714485438,
+          "pitch": 0.025437489465325314,
+          "rotation": 5.497787143782138,
+          "target": "1-"
+        },
+        {
+          "yaw": -0.15199339001856593,
+          "pitch": 0.011646290249879954,
+          "rotation": 6.283185307179586,
+          "target": "2-ok"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.8288195726895147,
+          "pitch": 0.014969356812134293,
+          "title": "35番出口",
+          "text": "動漫中反覆出現的地鐵出口"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "豐島區立東池袋中央公園",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.45353608585786986,
+          "pitch": 0.9269063785169784,
+          "rotation": 2.356194490192345,
+          "target": "0-35"
+        },
+        {
+          "yaw": 0.05964455464307861,
+          "pitch": 0.18808038828604623,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.209360981734715,
+          "pitch": 0.1432606283174831,
+          "title": "豐島區立東池袋中央公園",
+          "text": "第五集愛音被以前同學嗆到破防的公園"
+        }
+      ]
+    },
+    {
+      "id": "2-ok",
+      "name": "卡拉ok",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.16228600230962442,
+          "pitch": 0.024447529336018903,
+          "rotation": 6.283185307179586,
+          "target": "4-mixalive-tokyoring"
+        },
+        {
+          "yaw": -0.28917381070837145,
+          "pitch": 0.5759340303384413,
+          "rotation": 3.141592653589793,
+          "target": "0-35"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.072445712730719,
+          "pitch": -0.2027452395860756,
+          "title": "卡拉ok",
+          "text": "CRYCHIC回憶中去的那家卡拉OK"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "サウンドスタジオノア池袋店",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6228474235778236,
+          "pitch": 0.07766632180978483,
+          "rotation": 3.141592653589793,
+          "target": "4-mixalive-tokyoring"
+        },
+        {
+          "yaw": 3.0257231365255475,
+          "pitch": 0.1348894484784946,
+          "rotation": 3.141592653589793,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.83976393629284,
+          "pitch": -0.30276073088421285,
+          "title": "サウンドスタジオノア池袋店",
+          "text": "動漫中出現的大樓"
+        }
+      ]
+    },
+    {
+      "id": "4-mixalive-tokyoring",
+      "name": "Mixalive TOKYO(RiNG)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8175147738930235,
+          "pitch": 0.5369394953689,
+          "rotation": 3.141592653589793,
+          "target": "2-ok"
+        },
+        {
+          "yaw": 1.604724951363809,
+          "pitch": 0.17370616941421524,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.0743901653765526,
+          "pitch": -0.2745176223681405,
+          "title": "動漫中RiNG(livehouse)的原型",
+          "text": "動漫中RiNG(livehouse)的原型"
+        }
+      ]
+    }
+  ],
+  "name": "池袋",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
