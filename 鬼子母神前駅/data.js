62b6992dbf92aba4727f64afe94a3166d45fd6e9
rere98123-create <rere98123@gmail.com>
2025-11-13 10:39:29 +0800

    5

---
+++ 鬼子母神前駅/data.js
@@ -0,0 +1,266 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "鬼子母神站前",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.596168357161835,
+          "pitch": 0.19179611187928458,
+          "rotation": 6.283185307179586,
+          "target": "1-"
+        },
+        {
+          "yaw": -2.868263412261806,
+          "pitch": 0.16861278057355378,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.116072541240751,
+          "pitch": 0.01431154147480207,
+          "title": "主角高松燈家附近的車站",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "千登世小橋",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2534997993676242,
+          "pitch": 0.3290069719643487,
+          "rotation": 3.141592653589793,
+          "target": "0-"
+        },
+        {
+          "yaw": -0.01718845000840119,
+          "pitch": 0.1419396305775109,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.0870217244770881,
+          "pitch": 0.10779687513032599,
+          "title": "一樣經典場面之一--那傢伙竟敢無視燈。裡面的橋",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "千登世步橋",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.00883673412420194,
+          "pitch": 0.04709393531805972,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.00294341577482804,
+          "pitch": 0.4209722626479966,
+          "rotation": 3.141592653589793,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.22023920849535905,
+          "pitch": -0.1707905319603764,
+          "title": "經典場面之一的--我好想!成為人類啊! 裡面的那座天橋",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "学習院大学",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9439340600941275,
+          "pitch": 0.23238192667795943,
+          "rotation": 0.7853981633974483,
+          "target": "4-"
+        },
+        {
+          "yaw": 2.296693374077498,
+          "pitch": 0.8291299376888279,
+          "rotation": 3.141592653589793,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.3890519874979947,
+          "pitch": -0.035669194605683074,
+          "title": "動漫中，長崎爽世的學校-月之森的原型",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "富士見坂",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6929377626063493,
+          "pitch": 0.5584306494660858,
+          "rotation": 9.42477796076938,
+          "target": "0-"
+        },
+        {
+          "yaw": -2.9389674212374555,
+          "pitch": 0.49309723511273695,
+          "rotation": 3.141592653589793,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.875795280534195,
+          "pitch": 0.08727292545456677,
+          "title": "爽世追著祥子跑的那個坡道",
+          "text": "<br>"
+        }
+      ]
+    }
+  ],
+  "name": "鬼子母神站周邊",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
